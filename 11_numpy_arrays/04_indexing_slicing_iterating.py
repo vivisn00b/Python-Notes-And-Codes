751661bcbc3a76e402f86770a72f60a2d0b0786d
Viv <121664534+vivisn00b@users.noreply.github.com>
2025-12-19 20:23:59 +0530

    Numpy

--- 11_numpy_arrays/04_indexing_slicing_iterating.py
+++ 11_numpy_arrays/04_indexing_slicing_iterating.py
@@ -33,3 +33,29 @@ print(c[..., 2])  # same as c[:, :, 2]
 # to perform an operation on each element in the array, use the flat attribute which is an iterator over all the elements of the array
 for element in c.flat:
     print(element)
+
+# Boolean indexing
+arr = np.array([10, 15, 20, 25, 30])
+print(arr[arr > 15])
+
+# Fancy indexing / Advance indexing: access elements of an array by using another array or list of indices
+indices = [0, 2, 4]
+print(arr[indices])
+
+# Integer array indexing: similar to fancy indexing, this method allows us to access elements at specific, non-adjacent positions which makes it useful for extracting scattered data points.
+print(arr[[1, 2, 3]])
+
+# Ellipsis (...) in Indexing: ellipsis (...) can be used to select all dimensions which are not explicitly mentioned
+cube = np.random.rand(4, 4, 4)
+print(cube)
+print(cube[..., 0])
+
+# using np.newaxis to Add New Dimensions
+# np.newaxis keyword adds a new axis to the array which helps in converting a 1D array into a row or column vector
+array = np.array([1, 2, 3])
+print(array[:, np.newaxis])
+
+# Modifying Array Elements
+arr = np.array([1, 2, 3, 4])
+arr[1:3] = 99
+print(arr)

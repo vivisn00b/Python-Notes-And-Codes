987b14fe9720ba1b6304da440ccd91d096dac2bc
Viv <121664534+vivisn00b@users.noreply.github.com>
2026-01-13 23:44:04 +0530

    lambda, map, filter

--- 06_functions/04_built-in_functions.py
+++ 06_functions/04_built-in_functions.py
@@ -81,3 +81,35 @@ for x in [1, 2, 3]:
 # Replacing for loop:
 out = list(map(lambda x: x*2, [1, 2, 3]))
 print(out)
+
+
+# FILTER: used to extract elements from an iterable (like a list, tuple or set) that satisfy a given condition
+#
+def starts_a(w):
+    return w.startswith("a")
+li = ["apple", "banana", "avocado", "cherry", "apricot"]
+res = filter(starts_a, li)
+print(list(res))
+
+a = ["apple", "banana", "cherry", "kiwi", "grape"]
+b = filter(lambda w: len(w) > 5, a)
+print(list(b))
+
+# Using filter() with a Lambda Function
+a = [1, 2, 3, 4, 5, 6]
+b = filter(lambda x: x%2 == 0, a)
+print(list(b))
+
+# Filtering and Transforming Data
+a = [1, 2, 3, 4, 5, 6]
+b = filter(lambda x: x%2 == 0, a)
+c = map(lambda x: x*2, b)
+print(list(c))
+
+# Chaining with map()
+nums = [1, 2, 3, 4, 5]
+result = list(
+    map(lambda x: x*10,
+        filter(lambda x: x % 2 == 0, nums))
+)
+print(result)     # [20, 40]
